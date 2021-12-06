31c190def5e5dd7cbaa8648029af376f084eaab4
Odatas <46987568+Odatas@users.noreply.github.com>
2021-12-06 07:27:06 +0100

    Update day6.py
    Replaced Stupid Brute Force with smart brute force.

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,23 +1,39 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Dec  6 06:44:39 2021
+Created on Mon Dec  6 07:06:18 2021
 
-@author: Spongetheone
+@author: pphilipiak
 """
 
-lanternFish=[0]
-with open(r'''day6.txt''') as f:
-    lanternFishPopulation = [[int(x) for x in line.split(",")] for line in f][0]
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 06:44:39 2021
 
+@author: pphilipiak
+"""
+with open(r'''day6.txt''') as f:
+    calenderInput = [[int(x) for x in line.split(",")] for line in f][0]
 
 days = 256
+fishPopulation = []
+
+for i in range(9):
+    fishPopulation.append(calenderInput.count(i))
 
 for i in range(days):
-    print("Day:", i,"Number of Fish:",len(lanternFishPopulation))
-    for index in range(len(lanternFishPopulation)):
-        lanternFishPopulation[index] -=1
-        if lanternFishPopulation[index] < 0:
-            lanternFishPopulation[index] = 6
-            lanternFishPopulation.append(8)
+    tempList = []
+    for j in range(1,9):
+        tempList.append(fishPopulation[j])
 
-print("Part1:",len(lanternFishPopulation))
+    tempList.append(fishPopulation[0])
+    tempList[6] +=fishPopulation[0]
+    fishPopulation = tempList.copy()
+
+fishCounter = 0
+for i in fishPopulation:
+    fishCounter += i
+
+print(fishCounter)
+
+
+
