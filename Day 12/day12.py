2bacc196a0d29ef6df789b8e3cd8c82c5a97832e
Odatas <46987568+Odatas@users.noreply.github.com>
2021-12-12 12:26:13 +0100

    added comments

--- Day 12/day12.py
+++ Day 12/day12.py
@@ -13,39 +13,65 @@ def hasNoSmallDouble(liste):
             return False
     return True
 
-def findNextNodeToString(way):
+#Rekursion again because it goes brr
+def findNextNodeToString(way,withExtra = False):
+    #start with a list of nodes. The last one is the current node
     neighbors =G.adj[way[-1]]
-
+    #go through all neighbors
     for i in neighbors:
+        #If a neighbor is start. Skipp him
         if i == "start":
             continue
+        #if a neighbor is end we found a path so save him for late
         if i == "end":
             temp2 = [x for x in way]
             temp2.append(i)
             paths.append(temp2)
             #print("Found:",temp2)
             continue
+        #if the neighber is lowercase special rules aplly
         if i.islower():
-            if way.count(i) < 1 or (way.count(i) < 2 and hasNoSmallDouble(way)):
-                #print("Pass:",i,way)
-                pass
+            #2 modes: Allow 1 double visit of lowercase and allow no double visits
+            if withExtra:
+                if way.count(i) < 1 or (way.count(i) < 2 and hasNoSmallDouble(way)):
+                    pass
+                else:
+                    continue
             else:
-                #print("Blocked:",i,way)
-                continue
+                if way.count(i) > 0:
+                    continue
+        # when lowercase rule is ok append node to list and call function again
         tempList = [x for x in way]
         tempList.append(i)
-        findNextNodeToString(tempList)
-        
+        findNextNodeToString(tempList,withExtra)
+
 with open("day12.txt","r") as f:
     lines = f.read().split('\n')
-    
+#Get list of edges (those are the connections from one node to another)
 graph = [x.split("-") for x in lines ]
-
+#create graph
 G=nx.Graph()
+#fill graph withs edges. Yes its that easy. Python amazing.
 G.add_edges_from(graph)
+#draw graph just for the luls.
 nx.draw(G,with_labels = True)
 
+#creat list for all possible paths.
 paths=[]
+#add start to it
 way = ["start"]
-findNextNodeToString(way)
-print("Part1:",len(paths))
+#Recursion goes brrr
+findNextNodeToString(way,withExtra = False)
+#success
+print("Part 1:",len(paths))
+#do it again with weird extra rule
+paths=[]
+findNextNodeToString(way,withExtra = True)
+#great success
+print("Part 2:",len(paths))
+
+#submarine has great capabilitys maping all of the caves from the start position
+#That brings up question: who send me on this  mission?
+#My Memory is gone. I cant remeber anything before i was dropped in water
+#Yes functionallity of sub comes easy to me
+#Maybe i am spy? I have to be carefull who i talk to
