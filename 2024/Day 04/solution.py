f46a31ffa2ea749485d9c875c947dc69eb2ec65b
Odatas <46987568+Odatas@users.noreply.github.com>
2024-12-04 20:37:44 +0100

    Reformated

--- 2024/Day 04/solution.py
+++ 2024/Day 04/solution.py
@@ -1,7 +1,7 @@
 from advent_of_code_utility import read_file_to_list
 
 moves = [(-1, -1), (-1, 0), (-1, 1),  # Top-left, Top, Top-right
-         (0, -1), (0, 1),  # Left,         Right
+         (0, -1),           (0, 1),  # Left,         Right
          (1, -1), (1, 0), (1, 1)]  # Bottom-left, Bottom, Bottom-right
 
 # Idea: Regex
